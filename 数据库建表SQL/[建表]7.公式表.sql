d4aba1308fc7e223450db1223ee56d593b8b11d7
decrula <decrula@gmail.com>
2014-11-17 10:52:43 +0800

    公式表更新报警周期

--- 数据库建表SQL/[建表]7.公式表.sql
+++ 数据库建表SQL/[建表]7.公式表.sql
@@ -7,6 +7,7 @@ CREATE TABLE [dbo].[tz_Formula](												-- 公式引领表
 	[OrganizationID] [varchar](64) NULL,										-- 所属组织机构ID（公共：分厂，系统及自定义：生产线）
 	[Name] [nvarchar](50) NULL,													-- 公式组名称
 	[CreatedDate] [datetime] NULL,												-- 公式组创建日期
+	[AlarmPeriod] [int] DEFAULT(0) NULL,										-- 报警周期（分钟）
 	[Type] [int] DEFAULT(0) NULL,												-- 公式类型（1：公共，2：系统，3：自定义）
 	[State] [int] DEFAULT(0) NULL												-- 公式组状态
 )
@@ -31,7 +32,6 @@ GO
 
 CREATE TABLE [dbo].[formula_ConsumptionAlarmSetting](							-- 能耗报警设置
 	[KeyID] [uniqueidentifier] NULL,											-- 电耗报警公式引领ID
-	[AlarmPeriod] [int] DEFAULT(0) NULL,										-- 报警周期（分钟）
 	[CoalAlarmValue] [decimal](8, 2) NULL,										-- 煤耗报警值
 	[RelativeParameters] [nvarchar](MAX) NULL                                   -- 煤耗报警相关参数（DCS:标签名[,DCS:标签名]）
 )
