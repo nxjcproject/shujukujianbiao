b5e3438effa37f043793f743f5a5c882d627ea52
decrula <decrula@gmail.com>
2014-11-22 10:27:05 +0800

    组织机构表添加两列：海拔高度、生/熟料折算系数

--- 数据库建表SQL/[建表]3.生产机构及数据库映射表.sql
+++ 数据库建表SQL/[建表]3.生产机构及数据库映射表.sql
@@ -8,6 +8,8 @@ CREATE TABLE [dbo].[system_Organization](										-- 组织机构结构表
 	[DatabaseID] [uniqueidentifier] NULL,										-- 数据库ID
 	[Name] [nvarchar](50) NULL,													-- 生产机构名称
 	[Type] [nvarchar](50) NULL,													-- 生产线类型
+	[Altitude] [int] NULL,														-- 生产线海拔高度
+	[RawToClinkerCoff] [double] NULL,											-- 生/熟料这算系数
 	[ENABLED] [bit] NULL,														-- 是否有效
 	[LegalRepresentative] [nvarchar](30) NULL,									-- 法人代表
 	[Address] [nvarchar](max) NULL,												-- 生产机构地址
